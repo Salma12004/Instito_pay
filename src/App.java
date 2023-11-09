79e6f2c28a0325e54f2e31ab8dbe8344aa9627c3
Salma12004 <salmamghany@gmail.com>
2023-11-09 18:34:53 +0200

    Add program description, authors' names, and supervisor information to App.java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,11 @@
+// Program Description: Applying online payment bills and transfer money (InstitoPay System)
+// Last Modification Date: 9/11/2023
+// First author - ID : Salma Mohammed Mahmoud / 20210161
+// Second author - ID : Habiba Alaa Mohammed / 20210121
+// Third author - ID : Shahd Salah El-Dein / 20210183
+// Fourth author - ID : Shrouk Tarek Ibrahim / 20210175
+// Under The Supervision of: Dr. Soha Makady
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
